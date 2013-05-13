0a847234bfe65cc803f5bb4b794f1d6a5680eb04
chandru-balekkala <chandru.balekkala@iicorporate.com>
2013-05-13 19:49:53 +0530

    Added Employee Hierarchy UI.

---
+++ emp/employeeHierarchy/usr/defs.js
@@ -0,0 +1,48 @@
+ii.Import("fin.cmn.usr.defs");
+
+ii.init.register( function() {
+	
+    fin.emp = { hierarchy: {} };
+    
+}, 1);
+
+ii.init.register( function() { 
+	
+	fin.emp.hierarchy.jobArgs = {
+		id: {type: Number}
+		, title: {type: String, required: false, defaultValue: ""}
+	};
+
+	fin.emp.hierarchy.employeeArgs = {
+		id: {type: Number}
+		, employeeId: {type: Number}
+		, managerId: {type: Number}
+		, jobTitle: {type: String, required: false, defaultValue: ""}
+		, employeeName: {type: String, required: false, defaultValue: ""}
+		, orgLevel: {type: Number, required: false, defaultValue: 0}
+		, childNodeCount: {type: Number, required: false, defaultValue: 0}
+		, status: {type: Number, required: false, defaultValue: 0}
+		, modified: {type: Boolean, required: false, defaultValue: false}
+	};	
+
+}, 2);
+
+ii.Class({
+	Name: "fin.emp.hierarchy.Job",
+	Definition: {
+		init: function() {
+			var args = ii.args(arguments, fin.emp.hierarchy.jobArgs);
+			$.extend(this, args);
+		}
+	}
+});
+
+ii.Class({
+	Name: "fin.emp.hierarchy.Employee",
+	Definition: {
+		init: function() {
+			var args = ii.args(arguments, fin.emp.hierarchy.employeeArgs);
+			$.extend(this, args);
+		}
+	}
+});
