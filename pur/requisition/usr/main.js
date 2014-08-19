95d9f75bd541f1349b0b390095cc1f6e8e44d0fa
kiran-kommi <kiran.kommi@iicorporate.com>
2014-08-20 00:38:43 +0530

    Modified Item Grid column widths in Purchasing - PO Requisition module.

--- pur/requisition/usr/main.js
+++ pur/requisition/usr/main.js
@@ -700,14 +700,14 @@ ii.Class({
 				else
 				    return "<center><input type=\"checkbox\" id=\"selectInputCheck" + index + "\" class=\"iiInputCheck\" onchange=\"parent.fin.appUI.modified = true; fin.pur.poRequisitionUi.calculateTotal(this);\" /></center>";
             });
-			me.itemGrid.addColumn("number", "number", "Item Number", "Item Number", 110, null, me.itemNumber);
+			me.itemGrid.addColumn("number", "number", "Item Number", "Item Number", 120, null, me.itemNumber);
 			me.itemGrid.addColumn("description", "description", "Item Description", "Item Description", null, null, me.itemDescription);
 			me.itemGrid.addColumn("alternateDescription", "alternateDescription", "Alternate Description", "Alternate Description", 180, null, me.alternateDescription);
-			me.itemGrid.addColumn("account", "account", "GL Account No", "GL Account No", 120, function( account ) { return account.code + " - " + account.name; }, me.account);
-			me.itemGrid.addColumn("unit", "unit", "UOM", "UOM", 50, null, me.uom);
-			me.itemGrid.addColumn("manufactured", "manufactured", "Manufactured", "Manufactured", 120, null, me.manufactured);
+			me.itemGrid.addColumn("account", "account", "Gl Account No", "Gl Account No", 150, function( account ) { return account.code + " - " + account.name; }, me.account);
+			me.itemGrid.addColumn("unit", "unit", "UOM", "UOM", 80, null, me.uom);
+			me.itemGrid.addColumn("manufactured", "manufactured", "Mfg", "Mfg", 50, null, me.manufactured);
 			me.itemGrid.addColumn("quantity", "quantity", "Quantity", "Quantity", 80, null, me.quantity);
-			me.itemGrid.addColumn("price", "price", "Price", "Price", 70, null, me.price);
+			me.itemGrid.addColumn("price", "price", "Price", "Price", 80, null, me.price);
 			me.itemGrid.addColumn("extendedPrice", "", "Extended Price", "Extended Price", 120, function(data) {				
 				if (!isNaN(data.quantity) && data.price != undefined)
 					return ui.cmn.text.money.format(data.quantity * data.price);
@@ -719,14 +719,14 @@ ii.Class({
 				allowAdds: false			
 			});
 			
-			me.itemReadOnlyGrid.addColumn("number", "number", "Item Number", "Item Number", 110);
+			me.itemReadOnlyGrid.addColumn("number", "number", "Item Number", "Item Number", 120);
 			me.itemReadOnlyGrid.addColumn("description", "description", "Item Description", "Item Description", null);
 			me.itemReadOnlyGrid.addColumn("alternateDescription", "alternateDescription", "Alternate Description", "Alternate Description", 180);
-			me.itemReadOnlyGrid.addColumn("account", "account", "GL Account No", "GL Account No", 120, function(account) { return account.code + " - " + account.name;	});
-			me.itemReadOnlyGrid.addColumn("unit", "unit", "UOM", "UOM", 50);
-			me.itemReadOnlyGrid.addColumn("manufactured", "manufactured", "Manufactured", "Manufactured", 120);
+			me.itemReadOnlyGrid.addColumn("account", "account", "Gl Account No", "Gl Account No", 150, function(account) { return account.code + " - " + account.name;	});
+			me.itemReadOnlyGrid.addColumn("unit", "unit", "UOM", "UOM", 80);
+			me.itemReadOnlyGrid.addColumn("manufactured", "manufactured", "Mfg", "Mfg", 50);
 			me.itemReadOnlyGrid.addColumn("quantity", "quantity", "Quantity", "Quantity", 80);
-			me.itemReadOnlyGrid.addColumn("price", "price", "Price", "Price", 70);
+			me.itemReadOnlyGrid.addColumn("price", "price", "Price", "Price", 80);
 			me.itemReadOnlyGrid.addColumn("extendedPrice", "", "Extended Price", "Extended Price", 120, function(data) {				
 				if (!isNaN(data.quantity) && data.price != undefined)
 					return ui.cmn.text.money.format(data.quantity * data.price);
