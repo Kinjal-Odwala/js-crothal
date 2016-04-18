e577d748f6c055ef09d2cde2bbb0036c11ce38f7
nagamani-kalla <nagamani.kalla@iicorporate.com>
2016-04-18 20:14:59 +0530

    changed the audit option order in the status drop down of PAF.

--- emp/employeePAF/usr/fin.emp.employeePaf.js
+++ emp/employeePAF/usr/fin.emp.employeePaf.js
@@ -1687,9 +1687,9 @@ paf.controller('pafListCtrl', ['$scope', 'EmpActions', '$filter', '$sce', '$moda
 		{ id: 1, title: 'Open' },
 		{ id: 2, title: 'In Process' },
 		{ id: 8, title: 'Approved' },
+        { id: 11, title: 'Audited' },
 		{ id: 6, title: 'Cancelled' },
-		{ id: 10, title: 'Unapproved' },
-        { id: 11, title: 'Audited' }
+		{ id: 10, title: 'Unapproved' }
     ];
 
     $scope.getStatusTitle = function (id) {
