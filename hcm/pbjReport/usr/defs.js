f471aed450697342b27b13106ca973da95a46cac
chandru-balekkala <chandru.balekkala@iicorporate.com>
2016-04-30 05:58:25 +0530

    Ability to generate PBJ report based on the year and quarter and download the XML file.

--- hcm/pbjReport/usr/defs.js
+++ hcm/pbjReport/usr/defs.js
@@ -7,12 +7,43 @@ ii.init.register( function() {
 
 ii.init.register( function() {
 
+	fin.hcm.pbjReport.yearArgs = {
+		id: {type: Number}
+		, title: {type: String, required: false, defaultValue: ""}
+	};
+
+	fin.hcm.pbjReport.quarterArgs = {
+		id: {type: Number}
+		, title: {type: String, required: false, defaultValue: ""}
+	};
+
 	fin.hcm.pbjReport.fileNameArgs = {
 		id: {type: Number}
 		, fileName: {type: String, required: false, defaultValue: ""}
+		, houseCode: {type: String, required: false, defaultValue: ""}
 	};
 
 }, 2);
+
+ii.Class({
+	Name: "fin.hcm.pbjReport.Year",
+	Definition: {
+		init: function() {
+			var args = ii.args(arguments, fin.hcm.pbjReport.yearArgs);
+			$.extend(this, args);
+		}
+	}
+});
+
+ii.Class({
+	Name: "fin.hcm.pbjReport.Quarter",
+	Definition: {
+		init: function() {
+			var args = ii.args(arguments, fin.hcm.pbjReport.quarterArgs);
+			$.extend(this, args);
+		}
+	}
+});
 
 ii.Class({
 	Name: "fin.hcm.pbjReport.FileName",
--- hcm/pbjReport/usr/main.js
+++ hcm/pbjReport/usr/main.js
@@ -2,6 +2,7 @@ ii.Import( "ii.krn.sys.ajax" );
 ii.Import( "ii.krn.sys.session" );
 ii.Import( "ui.ctl.usr.input" );
 ii.Import( "ui.ctl.usr.buttons" );
+ii.Import( "ui.ctl.usr.grid" );
 ii.Import( "fin.cmn.usr.util" );
 ii.Import( "fin.hcm.pbjReport.usr.defs" );
 
@@ -10,6 +11,8 @@ ii.Style( "fin.cmn.usr.common", 2 );
 ii.Style( "fin.cmn.usr.statusBar", 3 );
 ii.Style( "fin.cmn.usr.input", 4 );
 ii.Style( "fin.cmn.usr.button", 5 );
+ii.Style( "fin.cmn.usr.dropDown", 6 );
+ii.Style( "fin.cmn.usr.grid", 7 );
 
 ii.Class({
     Name: "fin.hcm.pbjReport.UserInterface",
@@ -18,13 +21,15 @@ ii.Class({
 			var me = this;
 
 			me.loadCount = 1;
+			me.years = [];
+			me.quarters = [];
 
 			me.gateway = ii.ajax.addGateway("hcm", ii.config.xmlProvider);
 			me.cache = new ii.ajax.Cache(me.gateway);
 			me.transactionMonitor = new ii.ajax.TransactionMonitor(me.gateway, function(status, errorMessage){
 				me.nonPendingError(status, errorMessage);
 			});
-			
+
 			me.validator = new ui.ctl.Input.Validation.Master();
 			me.authorizer = new ii.ajax.Authorizer( me.gateway );
 			me.authorizePath = "\\crothall\\chimes\\fin\\HouseCodeSetup\\CMS/PBJReport";
@@ -38,16 +43,17 @@ ii.Class({
 
 			me.defineFormControls();
 			me.configureCommunications();
-			me.setStatus("Loading");	
+			me.setStatus("Loading");
 			me.modified(false);
+			me.initialize();
 
 			$(window).bind("resize", me, me.resize);
 
 			if (top.ui.ctl.menu) {
 				top.ui.ctl.menu.Dom.me.registerDirtyCheck(me.dirtyCheck, me);
 			}
-		},	
-		
+		},
+
 		authorizationProcess: function() {
 			var me = this;
 
@@ -66,29 +72,75 @@ ii.Class({
 				ii.timer.timing("Page displayed");
 				me.session.registerFetchNotify(me.sessionLoaded,me);
 				me.checkLoadCount();
-			}				
+			}
 			else
 				window.location = ii.contextRoot + "/app/usr/unAuthorizedUI.htm";
-		},	
-		
+		},
+
 		sessionLoaded: function() {
 
 			ii.trace("Session Loaded", ii.traceTypes.Information, "Session");
 		},
-		
-		resize: function() {
 
-			$("#ContainerArea").height($(window).height() - 115);
+		resize: function() {
+			var me = this;
+			$("#PBJReportContainer").height($(window).height() - 120);
+			me.fileNameGrid.setHeight($(window).height() - 110);
 		},
-		
+
 		defineFormControls: function() {
 			var me = this;
+
+			me.year = new ui.ctl.Input.DropDown.Filtered({
+				id: "Year",
+				formatFunction: function( type ) { return type.title; }
+		    });
+
+			me.year.makeEnterTab()
+				.setValidationMaster(me.validator)
+				.addValidation(ui.ctl.Input.Validation.required)
+				.addValidation(function( isFinal, dataMap) {
+
+				if (me.year.indexSelected === -1)
+					this.setInvalid("Please select the correct Year.");
+			});
+
+			me.quarter = new ui.ctl.Input.DropDown.Filtered({
+				id: "Quarter",
+				formatFunction: function( type ) { return type.title; }
+		    });
+
+			me.quarter.makeEnterTab()
+				.setValidationMaster(me.validator)
+				.addValidation(ui.ctl.Input.Validation.required)
+				.addValidation(function( isFinal, dataMap) {
+
+				if (me.quarter.indexSelected === -1)
+					this.setInvalid("Please select the correct Quarter.");
+			});
+
+			me.fileNameGrid = new ui.ctl.Grid({
+				id: "FileNameGrid",
+				appendToId: "divForm"
+			});
+
+			me.fileNameGrid.addColumn("houseCode", "houseCode", "House Code", "House Code", 120);
+			me.fileNameGrid.addColumn("fileName", "fileName", "File Name", "File Name", null);
+			me.fileNameGrid.capColumns();
 
 			me.anchorGenerate = new ui.ctl.buttons.Sizeable({
 				id: "AnchorGenerate",
 				className: "iiButton",
 				text: "<span>&nbsp;&nbsp;Generate XML&nbsp;&nbsp;</span>",
 				clickFunction: function() { me.actionGenerateItem(); },
+				hasHotState: true
+			});
+
+			me.anchorDownload = new ui.ctl.buttons.Sizeable({
+				id: "AnchorDownload",
+				className: "iiButton",
+				text: "<span>&nbsp;&nbsp;Download&nbsp;&nbsp;</span>",
+				clickFunction: function() { me.actionDownloadItem(); },
 				hasHotState: true
 			});
 		},
@@ -106,28 +158,27 @@ ii.Class({
 		},
 
 		setStatus: function(status) {
-			var me = this;
 
 			fin.cmn.status.setStatus(status);
 		},
 
 		dirtyCheck: function(me) {
-				
+
 			return !fin.cmn.status.itemValid();
 		},
-		
+
 		modified: function() {
 			var args = ii.args(arguments, {
 				modified: {type: Boolean, required: false, defaultValue: true}
 			});
 			var me = this;
-			
+
 			parent.fin.appUI.modified = args.modified;
 			if (args.modified)
 				me.setStatus("Edit");
 		},
-		
-		setLoadCount: function(me, activeId) {
+
+		setLoadCount: function() {
 			var me = this;
 
 			me.loadCount++;
@@ -135,7 +186,7 @@ ii.Class({
 			$("#messageToUser").text("Loading");
 			$("#pageLoading").fadeIn("slow");
 		},
-		
+
 		checkLoadCount: function() {
 			var me = this;
 
@@ -146,26 +197,61 @@ ii.Class({
 			}
 		},
 
+		initialize: function() {
+			var me = this;
+
+			me.years.push(new fin.hcm.pbjReport.Year(1, "2016"));
+			me.quarters.push(new fin.hcm.pbjReport.Quarter(1, "1 (October 1 - December 31)"));
+			me.quarters.push(new fin.hcm.pbjReport.Quarter(2, "2 (January 1 - March 31)"));
+			me.quarters.push(new fin.hcm.pbjReport.Quarter(3, "3 (April 1 - June 30)"));
+			me.quarters.push(new fin.hcm.pbjReport.Quarter(4, "4 (July 1 - September 30)"));
+
+			me.year.setData(me.years);
+			me.quarter.setData(me.quarters);
+			me.year.select(0, me.year.focused);
+			me.quarter.select(0, me.quarter.focused);
+		},
+
 		actionGenerateItem: function() {
 			var me = this;
+
+			me.validator.forceBlur();
+			// Check to see if the data entered is valid
+			if (!me.validator.queryValidity(true)) {
+				alert("In order to generate, the errors on the page must be corrected.");
+				return false;
+			}
 
 			me.setLoadCount();
 			me.setStatus("Generating");
 			$("#messageToUser").text("Generating XML Files");
 
 			me.fileNameStore.reset();
-			me.fileNameStore.fetch("userId:[user],type:pbj", me.fileNamesLoaded, me);
+			me.fileNameStore.fetch("userId:[user],type:pbj,year:" + me.years[me.year.indexSelected].title + ",quarter:" + me.quarters[me.quarter.indexSelected].id, me.fileNamesLoaded, me);
 		},
-		
+
 		fileNamesLoaded: function(me, activeId) {
 
-			me.checkLoadCount();
-			alert("PBJ XML files generated successfully.")
+			for (var index = 0; index < me.fileNames.length; index++) {
+				me.fileNames[index].houseCode = me.fileNames[index].fileName.substring(0, me.fileNames[index].fileName.indexOf("_"));
+			}
 
-//			if (me.fileNames.length == 1) {
-//				$("iframe")[0].contentWindow.document.getElementById("FileName").value = me.fileNames[0].fileName;
-//				$("iframe")[0].contentWindow.document.getElementById("DownloadButton").click();
-//			}
+			me.fileNameGrid.setData(me.fileNames);
+			me.checkLoadCount();
+
+			if (me.fileNames.length > 0)
+				alert("PBJ XML files generated successfully.");
+			else
+				alert("No records found to generate XML files.");
+		},
+
+		actionDownloadItem: function() {
+			var me = this;
+
+			if (me.fileNameGrid.activeRowIndex >= 0) {
+				$("iframe")[0].contentWindow.document.getElementById("FileName").value = me.fileNames[me.fileNameGrid.activeRowIndex].fileName;
+				$("iframe")[0].contentWindow.document.getElementById("DownloadButton").click();
+			}
 		}
 	}
 });
